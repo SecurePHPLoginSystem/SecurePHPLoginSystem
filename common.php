9c4021cf07cc6fcbfb7134128a435b5dfd7c2bc4
Nilpo <devnilpo@gmail.com>
2014-08-06 23:05:05 -0400

    Removing references to magic_quotes_gpc. The password_hash function used elsewhere requires PHP 5.5. Support for magic_quotes_gpc was removed in PHP 5.4 making all references are unnecessary.

--- common.php
+++ common.php
@@ -45,33 +45,6 @@
     // represents the name of the column in your database.
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    // This block of code is used to undo magic quotes.  Magic quotes are a terrible
-    // feature that was removed from PHP as of PHP 5.4.  However, older installations
-    // of PHP may still have magic quotes enabled and this code is necessary to
-    // prevent them from causing problems.  For more information on magic quotes:
-    // http://php.net/manual/en/security.magicquotes.php
-    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc())
-    {
-        function undo_magic_quotes_gpc(&$array)
-        {
-            foreach($array as &$value)
-            {
-                if(is_array($value))
-                {
-                    undo_magic_quotes_gpc($value);
-                }
-                else
-                {
-                    $value = stripslashes($value);
-                }
-            }
-        }
-
-        undo_magic_quotes_gpc($_POST);
-        undo_magic_quotes_gpc($_GET);
-        undo_magic_quotes_gpc($_COOKIE);
-    }
-
     // This tells the web browser that your content is encoded using UTF-8
     // and that it should submit content back to you using UTF-8
     header('Content-Type: text/html; charset=utf-8');
